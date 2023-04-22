bd5841f43a7b2b6fdf19ff229bcb957c20bd20fa
ompanchwate2003 <ompanchwate2003@gmail.com>
2023-04-23 00:44:58 +0530

    Updated

--- pages/dao-register.js
+++ pages/dao-register.js
@@ -1,8 +1,41 @@
-export default function Register(){
-    return(
-        <div>
-            <p>Register the new dao</p>
-        </div>
+import { Navbar } from "@/components";
+
+export default function Register() {
+    return (
+        <>
+            <Navbar />
+            <div className="bg-[#212121] h-screen flex justify-center items-start">
+                <form action="" className="pt-40 justify-center items-center space-y-6  ">
+                    <div className="flex space-x-9 ">
+                        <div>
+                            <h1 className="text-lg">DAO Name</h1>
+                            <input color="blue" placeholder="Enter DAO Name" className="w-[20rem] p-2 rounded-xl bg-slate-700 outline-none px-4" />
+                        </div>
+                        <div>
+                            <div>
+                                <h1 className="text-lg">DAO Owner</h1>
+                                <input color="blue" placeholder="Enter DAO Name" className="text-md w-[20rem] p-2 rounded-xl bg-slate-700 outline-none px-4" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex space-x-9 ">
+                        <div>
+                            <h1 className="text-lg">Date & Time</h1>
+                            <input type="datetime-local" color="blue" placeholder="Enter DAO Name" className="w-[20rem] p-2 rounded-xl bg-slate-700 outline-none px-4" />
+                        </div>
+                        <div>
+                            <div>
+                                <h1 className="text-lg">DAO Owner</h1>
+                                <input color="blue" placeholder="Enter DAO Name" className="text-md w-[20rem] p-2 rounded-xl bg-slate-700 outline-none px-4" />
+                            </div>
+                        </div>
+                    </div>
+
+
+                </form>
+            </div>
+        </>
     )
 
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import home_img from '../public/home.jpg'
+import { Navbar } from '@/components'
+import Link from 'next/link'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      Home
-      <ConnectButton />
-    </div>
+    <>
+      <Navbar />
+      <div className=" bg-[#212121] pb-16 top-0 absolute flex flex-col-reverse md:grid md:grid-cols-2 pt-32 justify-between  items-center  ">
+        <div className='grid-cols-1 flex flex-col justify-center mx-8 sm:mx-28 '>
+          <h1 className='leading-tight text-4xl lg:text-6xl font-bold'>Meet Without Worrying about Language</h1>
+          <p className=' mt-8 text-lg text-justify '>Use Moment to get new experience in doing video meet, don't care about language, you can set it in the Settings secion</p>
+
+          <div className='mt-8 space-x-10 flex  '>
+            <Link href='/meet'><button className='p-[0.6rem] px-7 rounded-3xl border border-white bg-[#fe7639] hover:bg-transparent text-lg'>Go to Meet</button></Link>
+            <button className='text-lg hover:underline hover:underline-offset-4'>Register for DAO</button>
+          </div>
+        </div>
+
+        <div className='grid-cols-1 flex justify-center items-center' ><Image src={home_img} className='h-[80%] w-[80%]' >
+        </Image></div>
+      </div>
+
+
+
+    </>
   )
 }
