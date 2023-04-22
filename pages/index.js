f9419988281472c9c6c811159f6cab74890484b4
Durvesh7k <dschopade7k@gmail.com>
2023-04-23 01:26:10 +0530

    change in the ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useRecording } from '@huddle01/react/dist/declarations/src/hooks';
+import {useRecorder} from '@huddle01/react/app-utils'
 
 
 const inter = Inter({ subsets: ['latin'] })
